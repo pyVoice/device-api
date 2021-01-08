c821a941ac2552fe2c0a0b2a0b6eb66e3758ca9d
Afonso Santos <afonso.faisca1@gmail.com>
2021-01-08 21:25:45 +0000

    Fixed (maybe) the MongoDB issues

--- device_api/settings/development.py
+++ device_api/settings/development.py
@@ -1,25 +1,33 @@
 from .base import *
 import environ
 
-
-environ.Env.read_env(
-	ALLOWED_HOSTS=(list, []),
+env = environ.Env(
+    DEBUG=(bool, True),
 )
 
-DEBUG = True
+environ.Env.read_env()
+
+DEBUG = env('DEBUG')
 
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'djongo',
-        'NAME': 'pyvoice-device-api',
+	# 'default': {
+	# 	'ENGINE': 'djongo',
+ #        'NAME': 'pyvoice-device-api',
+ #        'CLIENT': {
+ #            'host': '127.0.0.1',
+ #        }
+	# },
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'pyvoice-device-dev',
         'CLIENT': {
-            'host': '127.0.0.1',
+            'host': '***REMOVED***',
         }
-	}
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'device_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'device_api.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
