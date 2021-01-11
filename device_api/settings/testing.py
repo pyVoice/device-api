671af8a019185747cdf67ab7aae88738f532e277
Afonso Santos <afonso.faisca1@gmail.com>
2021-01-11 11:10:12 +0000

    Created testing settings module

---
+++ device_api/settings/testing.py
@@ -0,0 +1,12 @@
+from .base import *
+from django.core.management.utils import get_random_secret_key
+
+
+SECRET_KEY = get_random_secret_key()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'device-api-test'
+    }
+}
