aa0635c2c7a4a3fefcf6d46d83f97d1cbd273e19
Afonso Santos <afonso.faisca1@gmail.com>
2021-01-11 22:33:18 +0000

    Added comments

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -7,6 +7,7 @@ class TestViews(TestSetUp):
     """
 
     def test_cannot_register_device_without_data(self):
+        """ Test if a Device cannot be registered without providing any data """
         # configure Authorization header
         self.client.credentials(HTTP_AUTHORIZATION='Api-Key ' + self.key)
 
@@ -16,6 +17,7 @@ class TestViews(TestSetUp):
         self.assertEqual(response.status_code, 400)
 
     def test_cannot_register_device_withou_api_key(self):
+        """ Test if a Device cannot be registered without providing a API key """
         response = self.client.post(self.api_url)
 
         # 403 - Forbidden
