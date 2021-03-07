b2fb214230bc87985ef743592e4951fbbebbcc7d
Afonso Santos <afonso.faisca1@gmail.com>
2021-03-07 15:25:27 +0000

    Trying to fix collectstatic

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python3 manage.py migrate
 web: gunicorn device_api.wsgi --preload --log-file -
--- device_api/settings/production.py
+++ device_api/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 import os
 import environ
 import dj_database_url
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -52,12 +54,17 @@ REST_FRAMEWORK = {
     )
 }
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/assets/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+sentry_sdk.init(
+    dsn="https://b81a65d9c5f94adfb94ad01e47d9286f@o474195.ingest.sentry.io/5510129",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
 )
-
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
