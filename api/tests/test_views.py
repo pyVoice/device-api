70c5533b6cc4bb209a33f0d38cbf41a5d32b3a81
Afonso Santos <afonso.faisca1@gmail.com>
2021-04-10 13:11:06 +0100

    Removed unused test

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -19,22 +19,6 @@ class TestViews(TestSetUp):
         # expecting 400 - Bad Request
         self.assertEqual(response.status_code, 400)
 
-    def test_cannot_register_device_withou_api_key(self):
-        """ Test if a Device cannot be registered without providing a API key """
-
-        self.device_data = {
-            "device_id": str(uuid4()),
-            "operating_system": "Test OS",
-            "location": "Django",
-            "version": str(randint(0, 5)),
-        }
-
-        # make the API request
-        response = self.client.post(self.api_url)
-
-        # expecting 403 - Forbidden
-        self.assertEqual(response.status_code, 403)
-
     def test_can_register_device(self):
         """ Test if a Device can be registered using a API key and data """
 
